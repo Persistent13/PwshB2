61884ce3b4b19928008a43aed012a818902731e6
Dakota Clark <dakota@allclarks.com>
2018-02-10 19:56:27 -0800

    Move away from static method calls.

--- Pwsh2.Api/B2API.cs
+++ Pwsh2.Api/B2API.cs
@@ -10,6 +10,9 @@ namespace PwshB2.Api
 {
     public class B2
     {
+        public B2(string userName, string password) => AuthorizeAccount(userName, password);
+        public B2(Account session) => SaveSessionData(session);
+
         /// <summary>
         /// Used to get authorization to the B2 service as well as various other API minutia.
         /// </summary>
@@ -22,44 +25,44 @@ namespace PwshB2.Api
         /// <exception cref="ArgumentException">Throw when the userName or password parameters are empty or null.</exception>
         /// <exception cref="B2HttpException">Thrown when there is an HTTP level issue communicating with the B2 service.</exception>
         /// <exception cref="B2Exception">Thrown when a B2 service issue occurs.</exception>
-        public static Account AuthorizeAccount (string userName, string password)
+        private void AuthorizeAccount (string userName, string password)
         {
             Guard.Against.NullOrEmpty(userName, nameof(userName));
             Guard.Against.NullOrEmpty(password, nameof(password));
-            var client = new RestClient(Constant.BaseApiUrl)
+            var client = new RestClient(Constant.Resouce.BaseUrlString)
             {
                 Authenticator = new HttpBasicAuthenticator(userName, password)
             };
             var req = new RestRequest()
             {
                 Method = Method.GET,
-                Resource = B2Resouce.AuthorizeAccount,
+                Resource = Constant.Resouce.AuthorizeAccount,
                 RequestFormat = DataFormat.Json
             };
             var responce = client.Execute<Account>(req);
             Guard.Against.RestSharpError(responce);
             Guard.Against.B2Error(responce);
-            return responce.Data;
+            SaveSessionData(responce.Data);
         }
 
         /// <summary>
         /// This needs to be run in order to set the credentials to use for all B2 actions
         /// </summary>
         /// <param name="account">The account object returned by <c>AuthorizeAccount</c></param>
-        public static void SaveSessionData (Account account) => Session.Instance.accountSession = account;
+        private void SaveSessionData (Account account) => Session.Instance.accountSession = account;
 
         /// <summary>
         /// Used to return all buckets associated with an account.
         /// </summary>
         /// <param name="type">The bucket type to return.</param>
-        /// <returns></returns>
-        public static List<Bucket> ListBuckets (BucketType type)
+        /// <returns>A list of buckets, if any are present.</returns>
+        public List<Bucket> ListBuckets (BucketType type)
         {
             var body = new Dictionary<string, string>
             {
                 { "accountId", Session.Instance.accountSession.AccountId }
             };
-            return ExecuteB2Request<DtoBuckets>(body, B2Resouce.ListBuckets).Buckets;
+            return ExecuteB2Request<DtoBuckets>(body, Constant.Resouce.ListBuckets).Buckets;
         }
 
         /// <summary>
@@ -67,8 +70,8 @@ namespace PwshB2.Api
         /// </summary>
         /// <param name="name">The new name of the bucket. A name must be alphanumeric, at least six characters, and a max of fifty characters.</param>
         /// <param name="type">Only public and private types are valid when creating a bucket.</param>
-        /// <returns></returns>
-        public static Bucket CreateBucket (string name, BucketType type)
+        /// <returns>The updated bucket.</returns>
+        public Bucket CreateBucket (string name, BucketType type)
         {
             Guard.Against.B2NameError(name);
             Guard.Against.B2BucketTypeError(type);
@@ -79,10 +82,15 @@ namespace PwshB2.Api
                 { "bucketName", name },
                 { "bucketType", HashMap.FromBucketType[type] }
             };
-            return ExecuteB2Request<Bucket>(body, B2Resouce.CreateBucket);
+            return ExecuteB2Request<Bucket>(body, Constant.Resouce.CreateBucket);
         }
 
-        public static void SetBucketType (string name, BucketType type)
+        /// <summary>
+        /// Change the bucket to private or public.
+        /// </summary>
+        /// <param name="name">The new name of the bucket. A name must be alphanumeric, at least six characters, and a max of fifty characters.</param>
+        /// <param name="type">Only public and private types are valid when changing a bucket's type.</param>
+        public void SetBucketType (string name, BucketType type)
         {
             var body = new Dictionary<string, string>
             {
@@ -90,7 +98,7 @@ namespace PwshB2.Api
                 { "bucketType", HashMap.FromBucketType[type] },
                 { "bucketId", GetBucketIdFromName(name) }
             };
-            var responce = ExecuteB2Request<Bucket>(body, B2Resouce.UpdateBucket);
+            var responce = ExecuteB2Request<Bucket>(body, Constant.Resouce.UpdateBucket);
             if (responce.BucketType != HashMap.FromBucketType[type])
             {
                 throw new B2Exception($"Bucket {name} failed to update.");
@@ -100,11 +108,11 @@ namespace PwshB2.Api
         /// <summary>
         /// A helper method to run B2 REST requests.
         /// </summary>
-        /// <typeparam name="T">The DTO object to return.</typeparam>
-        /// <param name="body"></param>
-        /// <param name="resource"></param>
-        /// <returns></returns>
-        private static T ExecuteB2Request<T> (Dictionary<string, string> body, B2Resouce resource)
+        /// <typeparam name="T">The IDto interface object to return.</typeparam>
+        /// <param name="body">An all string Dictionary object the represents the body of a request. Unique per REST resource.</param>
+        /// <param name="resource">A B2Resource enumerable type that represents a B2 REST resource.</param>
+        /// <returns>An object based on the IDto interface.</returns>
+        private static T ExecuteB2Request<T> (Dictionary<string, string> body, Constant.Resouce resource)
             where T : IDto, new()
         {
             Guard.Against.Null(body, nameof(body));
@@ -127,36 +135,57 @@ namespace PwshB2.Api
             return resp.Data;
         }
 
-        public static void RemoveBucket (string name)
+        /// <summary>
+        /// Remove a bucket. The bucket cannot be restored.
+        /// </summary>
+        /// <param name="name">The new name of the bucket. A name must be alphanumeric, at least six characters, and a max of fifty characters.</param>
+        public void RemoveBucket (string name)
         {
             var body = new Dictionary<string, string>
             {
                 { "accountId", Session.Instance.accountSession.AccountId },
                 { "bucketId", GetBucketIdFromName(name) }
             };
-            ExecuteB2Request<Bucket>(body, B2Resouce.DeleteBucket);
+            ExecuteB2Request<Bucket>(body, Constant.Resouce.DeleteBucket);
         }
 
-        public static void SetCorsRules ()
+        public void SetCorsRules ()
         {
             throw new NotImplementedException();
         }
 
-        public static void SetLifecycleRules ()
+        public void SetLifecycleRules ()
         {
             throw new NotImplementedException();
         }
 
-        public static void DeleteBucket (List<Bucket> bucketsToDelete)
+        public List<File> ListFiles(string name, string filterPrefix)
         {
-            throw new NotImplementedException();
+            DtoFiles returnData;
+            var files = new List<File>();
+            var body = new Dictionary<string, string>
+            {
+                { "", "" }
+            };
+            string bucketId = GetBucketIdFromName(name);
+            if(!string.IsNullOrEmpty(filterPrefix))
+            {
+                body.Add(B2FileParameter.Prefix, filterPrefix);
+            }
+            do
+            {
+                returnData = ExecuteB2Request<DtoFiles>(body, Constant.Resouce.ListFileNames);
+                body.Remove("nextFileName");
+                body.Add("nextFileName", returnData.NextFileName);
+            } while (!string.IsNullOrEmpty(returnData.NextFileName));
+            return files;
         }
 
         internal static string GetBucketIdFromName (string bucketName)
         {
             try
             {
-                return ListBuckets(BucketType.All).Find(bucket => bucket.BucketName == bucketName).BucketId;
+                return new B2(Session.Instance.accountSession).ListBuckets(BucketType.All).Find(bucket => bucket.BucketName == bucketName).BucketId;
             }
             catch (NullReferenceException err)
             {
@@ -165,7 +194,7 @@ namespace PwshB2.Api
         }
     }
     //The session data, can be updated but not duplicated
-    internal sealed class Session
+    public sealed class Session
     {
         public Account accountSession { get; set; }
         private static readonly Lazy<Session> session = new Lazy<Session>(() => new Session());
--- Pwsh2.Api/Utilities.cs
+++ Pwsh2.Api/Utilities.cs
@@ -6,8 +6,41 @@ namespace PwshB2.Api
 {
     internal static class Constant
     {
-        public const string BaseApiUrl = "https://api.backblazeb2.com/";
         public const string Authorization = "Authorization";
+        internal class Resouce : SmartEnum<Resouce, int>
+        {
+            public static Resouce BaseUrlString = new Resouce("https://api.backblazeb2.com/", 0);
+            public static Resouce AuthorizeAccount = new Resouce("b2api/v1/b2_authorize_account", 1);
+            public static Resouce UpdateBucket = new Resouce("/b2api/v1/b2_update_bucket", 2);
+            public static Resouce CreateBucket = new Resouce("/b2api/v1/b2_create_bucket", 3);
+            public static Resouce ListBuckets = new Resouce("/b2api/v1/b2_list_buckets", 4);
+            public static Resouce CancelLargeFile = new Resouce("/b2api/v1/b2_cancel_large_file", 5);
+            public static Resouce DeleteBucket = new Resouce("/b2api/v1/b2_delete_bucket", 6);
+            public static Resouce DeleteFileVersion = new Resouce("/b2api/v1/b2_delete_file_version", 7);
+            public static Resouce DownloadFileById = new Resouce("/b2api/v1/b2_download_file_by_id", 8);
+            public static Resouce DownloadFileByName = new Resouce("/b2api/v1/b2_download_file_by_name", 9);
+            public static Resouce GetDownloadAuthorization = new Resouce("/b2api/v1/b2_get_download_authorization", 10);
+            public static Resouce FinishLargeFile = new Resouce("/b2api/v1/b2_finish_large_file", 11);
+            public static Resouce GetFileInfo = new Resouce("/b2api/v1/b2_get_file_info", 12);
+            public static Resouce GetUploadPartUrl = new Resouce("/b2api/v1/b2_get_upload_part_url", 13);
+            public static Resouce GetUploadUrl = new Resouce("/b2api/v1/b2_get_upload_url", 14);
+            public static Resouce HideFile = new Resouce("/b2api/v1/b2_hide_file", 15);
+            public static Resouce ListFileNames = new Resouce("/b2api/v1/b2_list_file_names", 16);
+            public static Resouce ListFileVersions = new Resouce("/b2api/v1/b2_list_file_versions", 17);
+            public static Resouce ListLargeFileParts = new Resouce("/b2api/v1/b2_list_parts", 18);
+            public static Resouce ListUnfinishedLargeFiles = new Resouce("/b2api/v1/b2_list_unfinished_large_files", 19);
+            public static Resouce StartLargeFile = new Resouce("/b2api/v1/b2_start_large_file", 20);
+            public static Resouce UploadFile = new Resouce("/b2api/v1/b2_upload_file", 21);
+            public static Resouce UploadLargeFilePart = new Resouce("/b2api/v1/b2_upload_part", 22);
+
+            protected Resouce(string name, int value) : base(name, value) { }
+
+            public override string ToString() => Name;
+            public static implicit operator Resouce(string name) => FromName(name);
+            public static implicit operator string(Resouce type) => type.ToString();
+            public static implicit operator Resouce(int value) => FromValue(value);
+            public static implicit operator int(Resouce type) => Int32.Parse(type);
+        }
     }
     public class HashMap
     {
@@ -26,6 +59,22 @@ namespace PwshB2.Api
             { BucketType.Snapshot, "snapshot" }
         };
     }
+    internal class B2FileParameter : SmartEnum<B2FileParameter, int>
+    {
+        public static B2FileParameter BucketId = new B2FileParameter("bucketId", 0);
+        public static B2FileParameter StartFileName = new B2FileParameter("startFileName", 1);
+        public static B2FileParameter MaxFileCount = new B2FileParameter("maxFileCount", 2);
+        public static B2FileParameter Prefix = new B2FileParameter("prefix", 3);
+        public static B2FileParameter Delimiter = new B2FileParameter("delimiter", 4);
+
+        protected B2FileParameter(string name, int value) : base(name, value) { }
+
+        public override string ToString() => Name;
+        public static implicit operator B2FileParameter(string name) => FromName(name);
+        public static implicit operator string(B2FileParameter type) => type.ToString();
+        public static implicit operator B2FileParameter(int value) => FromValue(value);
+        public static implicit operator int(B2FileParameter type) => Int32.Parse(type);
+    }
     public class BucketType : SmartEnum<BucketType, int>
     {
         public static BucketType All = new BucketType("All", 0);
@@ -40,39 +89,5 @@ namespace PwshB2.Api
         public static implicit operator string(BucketType type) => type.ToString();
         public static implicit operator BucketType(int value) => FromValue(value);
         public static implicit operator int(BucketType type) => Int32.Parse(type);
-    }
-    internal class B2Resouce : SmartEnum<B2Resouce, int>
-    {
-        public static B2Resouce BaseUrlString = new B2Resouce("https://api.backblazeb2.com/", 0);
-        public static B2Resouce AuthorizeAccount = new B2Resouce("b2api/v1/b2_authorize_account", 1);
-        public static B2Resouce UpdateBucket = new B2Resouce("/b2api/v1/b2_update_bucket", 2);
-        public static B2Resouce CreateBucket = new B2Resouce("/b2api/v1/b2_create_bucket", 3);
-        public static B2Resouce ListBuckets = new B2Resouce("/b2api/v1/b2_list_buckets", 4);
-        public static B2Resouce CancelLargeFile = new B2Resouce("/b2api/v1/b2_cancel_large_file", 5);
-        public static B2Resouce DeleteBucket = new B2Resouce("/b2api/v1/b2_delete_bucket", 6);
-        public static B2Resouce DeleteFileVersion = new B2Resouce("/b2api/v1/b2_delete_file_version", 7);
-        public static B2Resouce DownloadFileById = new B2Resouce("/b2api/v1/b2_download_file_by_id", 8);
-        public static B2Resouce DownloadFileByName = new B2Resouce("/b2api/v1/b2_download_file_by_name", 9);
-        public static B2Resouce GetDownloadAuthorization = new B2Resouce("/b2api/v1/b2_get_download_authorization", 10);
-        public static B2Resouce FinishLargeFile = new B2Resouce("/b2api/v1/b2_finish_large_file", 11);
-        public static B2Resouce GetFileInfo = new B2Resouce("/b2api/v1/b2_get_file_info", 12);
-        public static B2Resouce GetUploadPartUrl = new B2Resouce("/b2api/v1/b2_get_upload_part_url", 13);
-        public static B2Resouce GetUploadUrl = new B2Resouce("/b2api/v1/b2_get_upload_url", 14);
-        public static B2Resouce HideFile = new B2Resouce("/b2api/v1/b2_hide_file", 15);
-        public static B2Resouce ListFileNames = new B2Resouce("/b2api/v1/b2_list_file_names", 16);
-        public static B2Resouce ListFileVersions = new B2Resouce("/b2api/v1/b2_list_file_versions", 17);
-        public static B2Resouce ListLargeFileParts = new B2Resouce("/b2api/v1/b2_list_parts", 18);
-        public static B2Resouce ListUnfinishedLargeFiles = new B2Resouce("/b2api/v1/b2_list_unfinished_large_files", 19);
-        public static B2Resouce StartLargeFile = new B2Resouce("/b2api/v1/b2_start_large_file", 20);
-        public static B2Resouce UploadFile = new B2Resouce("/b2api/v1/b2_upload_file", 21);
-        public static B2Resouce UploadLargeFilePart = new B2Resouce("/b2api/v1/b2_upload_part", 22);
-
-        protected B2Resouce(string name, int value) : base(name, value) { }
-
-        public override string ToString() => Name;
-        public static implicit operator B2Resouce(string name) => FromName(name);
-        public static implicit operator string(B2Resouce type) => type.ToString();
-        public static implicit operator B2Resouce(int value) => FromValue(value);
-        public static implicit operator int(B2Resouce type) => Int32.Parse(type);
     }
 }
